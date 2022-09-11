3eda65d5a443a9f3a6e74fc1b89c86b006a4bc37
harksu <harksulim@naver.com>
2022-09-11 16:38:28 +0900

    Feat : Router set, InPutText, KeyboardEvent(SignUpPage)

--- Components/InputBox.js
+++ Components/InputBox.js
@@ -1,17 +1,52 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, TextInput, Keyboard } from "react-native";
+import React, { useState, useRef } from "react";
 
 const InputBox = () => {
+  const [name, setName] = useState("");
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState(""); //이거 나중에 서버에 보내면 그 때 다 clear
+
+  const idInputRef = useRef();
+  const pwInputRef = useRef();
+
   return (
     <View style={styles.container}>
       <View style={styles.box}>
-        <Text style={styles.text}>이름을 입력해주세요</Text>
+        <TextInput
+          placeholder="이름을 입력해주세요"
+          value={name}
+          onChangeText={setName}
+          textAlign={"center"}
+          autoFocus={true}
+          blurOnSubmit={false}
+          onSubmitEditing={() => {
+            idInputRef.current.focus();
+          }}
+        />
       </View>
       <View style={styles.box}>
-        <Text>사용하실 ID를 입력해주세요</Text>
+        <TextInput
+          placeholder="사용하실 ID를 입력해주세요"
+          value={id}
+          onChangeText={setId}
+          textAlign={"center"}
+          ref={idInputRef}
+          blurOnSubmit={false}
+          onSubmitEditing={() => pwInputRef.current.focus()}
+        />
       </View>
       <View style={styles.box}>
-        <Text>사용하실 PW를 입력해주세요</Text>
+        <TextInput
+          placeholder="사용하실 PW를 입력해주세요"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry={true}
+          placeholderTextColor="gray"
+          textAlign={"center"}
+          ref={pwInputRef}
+          blurOnSubmit={false}
+          onSubmitEditing={Keyboard.dismiss}
+        />
       </View>
     </View>
   );
@@ -31,6 +66,7 @@ const styles = StyleSheet.create({
     justifyContent: "space-around",
     width: "80%",
     flex: 0.25,
+    textAlign: "center",
     //나중에 포커스 될 때만 색깔 바꾸고
     borderWidth: 2,
     borderColor: "rgba(230, 230, 230, .5)",
