ca623474910d114ffa26762a96b415d7cafaa930
harksu <harksulim@naver.com>
2022-09-18 17:13:31 +0900

    Feat : adjust DrawerLayout (todo : adjust dropdown)

--- App.js
+++ App.js
@@ -16,9 +16,10 @@ const App = () => {
     <RecoilRoot>
       <NavigationContainer>
         <Stack.Navigator initialRouteName="SignUpScreen">
+          <Stack.Screen name="매칭페이지" component={MatchScreen} />
           <Stack.Screen name="회원가입" component={SignUp} />
           <Stack.Screen name="메인페이지" component={MainPage} />
-          <Stack.Screen name="매칭페이지" component={MatchScreen} />
+
           <Stack.Screen name="가이드리스트" component={GuideListPage} />
           <Stack.Screen name="디테일페이지" component={DetailPage} />
         </Stack.Navigator>
--- Components/MatchInputBox.js
+++ Components/MatchInputBox.js
@@ -1,48 +1,77 @@
-import { StyleSheet, Text, View, TextInput, Keyboard } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Keyboard,
+  DrawerLayoutAndroid,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 import React, { useState, useRef } from "react";
 
 const MatchInputBox = ({ locationInput }) => {
   const locationInputRef = useRef();
   const periodInputRef = useRef();
+  const drawer = useRef(null);
 
   const [location, setLocation] = useState("");
   const [period, setPeriod] = useState("");
   const [focusInput, setFocusInput] = useState(locationInputRef); // 사실 이거 필요없음
 
-  return (
+  const navigationView = () => (
     <View style={styles.container}>
-      <View style={styles.box}>
-        <TextInput
-          placeholder={locationInput}
-          value={location}
-          onChangeText={setLocation}
-          textAlign={"center"}
-          blurOnSubmit={false}
-          ref={locationInputRef}
-          onTouchStart={() => {
-            setFocusInput(locationInputRef);
-          }}
-          onSubmitEditing={() => {
-            periodInputRef.current.focus();
-            setFocusInput(periodInputRef);
-          }}
-        />
-      </View>
-      <View style={styles.box}>
-        <TextInput
-          placeholder="기간을 입력해주세요"
-          value={period}
-          onChangeText={setPeriod}
-          textAlign={"center"}
-          ref={periodInputRef}
-          blurOnSubmit={false}
-          onTouchStart={() => {
-            setFocusInput(periodInputRef);
-          }}
-          onSubmitEditing={Keyboard.dismiss}
-        />
-      </View>
+      <Text style={styles.paragraph}>I'm in the Drawer!</Text>
+      <Button
+        title="Close drawer"
+        onPress={() => drawer.current.closeDrawer()}
+      />
     </View>
+  );
+
+  return (
+    <DrawerLayoutAndroid
+      ref={drawer}
+      drawerWidth={300}
+      drawerPosition="left"
+      renderNavigationView={navigationView}
+      style={{ backgroundColor: "pink" }}
+    >
+      <View style={styles.container}>
+        <View style={styles.box}>
+          <TouchableOpacity onPress={() => drawer.current.openDrawer()}>
+            <Text>{locationInput} </Text>
+          </TouchableOpacity>
+          {/* <TextInput
+            placeholder={locationInput}
+            value={location}
+            onChangeText={setLocation}
+            textAlign={"center"}
+            blurOnSubmit={false}
+            ref={locationInputRef}
+            onSubmitEditing={() => {
+              periodInputRef.current.focus();
+              setFocusInput(periodInputRef);
+            }}
+          /> */}
+        </View>
+
+        <View style={styles.box}>
+          <TextInput
+            placeholder="기간을 입력해주세요"
+            value={period}
+            onChangeText={setPeriod}
+            textAlign={"center"}
+            ref={periodInputRef}
+            blurOnSubmit={false}
+            onTouchStart={() => {
+              setFocusInput(periodInputRef);
+            }}
+            onSubmitEditing={Keyboard.dismiss}
+          />
+        </View>
+      </View>
+    </DrawerLayoutAndroid>
   );
 };
 
